912c08f608d1e8daacf7e85ca09bbc637622894f
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-10 18:59:16 +0530

    Rendering a list using map

--- src/components/TopicList.js
+++ src/components/TopicList.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Topic from './Topic'
+import { topics } from '../data/data'
 
 const TopicList = () => {
+    const topicItems = topics.map(topic => (
+        <Topic name={topic.name} isCompleted={topic.isCompleted} key={topic.id} />
+    ));
     return (
         <section>
             <h1>List of ReactJS Topics</h1>
             <ul>
-                <Topic isCompleted={true} name="Class components" />
-                <Topic isCompleted={true} name="Functional components" />
-                <Topic isCompleted={true} name="JSX and Rendering" />
-                <Topic isCompleted={false} name="Rendering List" />
+                {topicItems}
             </ul>
         </section>
     )
---
+++ src/data/data.js
@@ -0,0 +1,6 @@
+export const topics=[
+    {id: 0, name:"Class components",isCompleted:true},
+    {id: 1, name:"Functional components",isCompleted:true},
+    {id: 2, name:"JSX and Rendering",isCompleted:true},
+    {id: 3, name:"Rendering List",isCompleted:false}
+];
