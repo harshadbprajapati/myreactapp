1b0f3e6c732ae225b625387afc84e4630ef02de9
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-10 18:33:11 +0530

    Use double curly braces

--- src/components/RulesList.js
+++ src/components/RulesList.js
@@ -7,7 +7,13 @@ const RulesList = () => {
         <img src="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=qc6CJjYAAAAJ&citpid=2"
             alt="Albert Einstein"
             className="profile-photo" />
-        <ul>
+        <ul style={
+                {
+                    backgroundColor: 'black',
+                    color: 'yellow'
+                }
+            }
+        >
             <li>Out of clutter, simplicity.</li>
             <li>From discord, find harmony.</li>
             <li>In the middle of difficulty, lies opportunity.</li>
