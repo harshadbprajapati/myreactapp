afd97699ed2ba6446a35ec8b00929bc887e98f61
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-11 19:44:47 +0530

    Add another state variable to show/hide details

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
-import { sculptureList } from '../data/data';
+import React, { useState } from "react";
+import { sculptureList } from "../data/data.js";
 
-const Gallery = () => {
-  const [index, setIndex] = useState(0)
-  console.log("Gallery: index: " + index);
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
-  function handleClick() {
-    setIndex(index+1);
-    console.log("handleClick: index: " + index);
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
-  
   return (
     <>
-      <button onClick={handleClick}>Next</button>
+      <button onClick={handleNextClick}>Next</button>
       <h2>
         <i>{sculpture.name} </i>
         by {sculpture.artist}
@@ -22,10 +24,13 @@ const Gallery = () => {
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
-      <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      <div>
+        {showMore && <p>{sculpture.description}</p>}
+        <img src={sculpture.url} alt={sculpture.alt} />
+      </div>
     </>
   );
 }
-
-export default Gallery;
