d7fce3245f6cc5d163303558b2c110ed912131e3
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-22 13:13:21 +0530

    Form submission with different controls

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -15,12 +15,27 @@ const MyForm = () => {
   const [state, setState] = useState(initialState);
 
   const handleChange = (event) => {
-    setState({...state, [event.target.name]: event.target.value});
+    const value = event.target.type === "checkbox" ? 
+      event.target.checked : 
+      event.target.value;
+    setState({...state, [event.target.name]: value});
   }
+
+  const handleSelectChange = (event) => {
+    const selectedOptions = Array.from(
+      event.target.selectedOptions,
+      (option) => option.value
+    );
+
+    setState({
+      ...state,
+      interests: selectedOptions,
+    });
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("First Name: ", event.currentTarget.elements[0].value);
-    console.log("Last Name: ", event.currentTarget.lastName.value);
+    console.log("Form data: ",state);
     return false;
   }
   return (
@@ -33,15 +48,17 @@ const MyForm = () => {
             type="text"
             id="firstName"
             name="firstName"
+            value={state.firstName}
             onChange={handleChange}
           />
         </div>
         <div>
-          <label htmlFor="lastName">First Name: </label>
+          <label htmlFor="lastName">Last Name: </label>
           <input
             type="text"
             id="lastName"
             name="lastName"
+            value={state.lastName}
             onChange={handleChange}
           />
         </div>
@@ -52,6 +69,7 @@ const MyForm = () => {
             id="maleGender"
             name="gender"
             value="Male"
+            checked={state.gender === "Male"}
             onChange={handleChange}
           />
           <label htmlFor="maleGender">Male</label>
@@ -60,9 +78,54 @@ const MyForm = () => {
             id="femaleGender"
             name="gender"
             value="Female"
+            checked={state.gender === "Female"}
             onChange={handleChange}
           />
           <label htmlFor="femaleGender">Female</label>
+        </div>
+        <div>
+          Known Languages:
+          <input
+            type="checkbox"
+            id="hindi"
+            name="knowsHindi"
+            checked={state.knowsHindi}
+            onChange={handleChange}
+          />
+          <label htmlFor="hindi">Hindi</label>
+          <input
+            type="checkbox"
+            id="gujarati"
+            name="knowsGujarati"
+            checked={state.knowsGujarati}
+            onChange={handleChange}
+          />
+          <label htmlFor="gujarati">Gujarati</label>
+          <input
+            type="checkbox"
+            id="english"
+            name="knowsEnglish"
+            checked={state.knowsEnglish}
+            onChange={handleChange}
+          />
+          <label htmlFor="english">English</label>
+        </div>
+
+        <div>
+          <label htmlFor="interests">Interests: </label>
+          <select
+            multiple
+            name="interests"
+            id="interests"
+            value={state.interests}
+            onChange={handleSelectChange}
+          >
+            {allInterests.map((interest) => (
+              <option value={interest} key={interest}>
+                {interest}
+              </option>
+            ))}
+          </select>
         </div>
 
         <button type="submit">Submit</button>
@@ -72,9 +135,15 @@ const MyForm = () => {
         <div>First Name: {state.firstName}</div>
         <div>Last Name: {state.lastName}</div>
         <div>Gender: {state.gender}</div>
+        <div>Knows Hindi: {"" + state.knowsHindi}</div>
+        <div>Knows Gujarati: {"" + state.knowsGujarati}</div>
+        <div>Knows English: {"" + state.knowsEnglish}</div>
+        <div>Interest: {"" + state.interests}</div>
       </div>
     </div>
   );
 }
 
-export default MyForm
+export default MyForm;
+
+
