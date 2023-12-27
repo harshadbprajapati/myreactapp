0dbfb1c0d27bf248320a1c5ffc337942f9554fd4
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-27 20:13:52 +0530

    setup react router in our react app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import Profile from "./components/Profile";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/home",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+]);
+
 
 function App() {
   return (
-    <div className='profiles'>
-      <Profile />
-      <Profile />
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
 
 export default App;
-
-
-// import LifeCycle from "./classcomponents/LifeCycle";
-
-// function App() {
-//   return (
-//     <div>
-//       Welcome to React
-//       <LifeCycle />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
