b1a10037f3ee1b5865237c6db454075b301bc949
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-30 06:49:29 +0530

    make navigation as layout route

--- src/App.js
+++ src/App.js
@@ -6,23 +6,33 @@ import AdminLayout from "./components/AdminLayout";
 function App() {
   return (
     <div>
-      <Navigation />
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/home" element={<div>Home</div>} />
-        <Route path="/about" element={<div>About</div>} />
-        <Route path="/teacher" element={<TeacherLayout />}>
-          <Route index element={<div>Teacher Overview</div>}></Route>
-          <Route path="/teacher/subjects" element={<div>Teacher Subjects</div>}></Route>
-          <Route path="/teacher/lectures" element={<div>Teacher Lectures</div>}></Route>
-          <Route path="/teacher/plan" element={<div>Teacher Plan</div>}></Route>
+        <Route element={<Navigation />}>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/home" element={<div>Home</div>} />
+          <Route path="/about" element={<div>About</div>} />
+          <Route path="/teacher" element={<TeacherLayout />}>
+            <Route index element={<div>Teacher Overview</div>}></Route>
+            <Route
+              path="/teacher/subjects"
+              element={<div>Teacher Subjects</div>}
+            ></Route>
+            <Route
+              path="/teacher/lectures"
+              element={<div>Teacher Lectures</div>}
+            ></Route>
+            <Route path="/teacher/plan" element={<div>Teacher Plan</div>}></Route>
+          </Route>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<div>Admin Overview</div>}></Route>
+            <Route path="/admin/add-staff" element={<div>Add Staff</div>}></Route>
+            <Route
+              path="/admin/delete-staff"
+              element={<div>Delete Staff</div>}
+            ></Route>
+          </Route>
+          <Route path="/*" element={<div>Page Not Found</div>} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<div>Admin Overview</div>}></Route>
-          <Route path="/admin/add-staff" element={<div>Add Staff</div>}></Route>
-          <Route path="/admin/delete-staff" element={<div>Delete Staff</div>}></Route>
-        </Route>
-        <Route path="/*" element={<div>Page Not Found</div>} />
       </Routes>
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav>
-      <Link to={"/home"}>Home</Link>
-      <Link to={"/about"}>About</Link>
-      <Link to={"/teacher"}>Teacher</Link>
-      <Link to={"/admin"}>Admin</Link>
-    </nav>
+    <>
+      <nav>
+        <Link to={"/home"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/teacher"}>Teacher</Link>
+        <Link to={"/admin"}>Admin</Link>
+      </nav>
+      <Outlet />
+    </>
   )
 }
 
