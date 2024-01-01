1d71601175e5607e22f48539536f02b6062d7d8f
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-01-01 09:39:16 +0530

    use redux toolkit's configure store method

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,9 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production", // Enable Redux DevTools in development
+});
 
 export default store;
-
-
