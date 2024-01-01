9b6d2467b41b67dc3571e9a636154ecc284b9506
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-01-01 21:22:21 +0530

    add logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Tuple } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: () => new Tuple(logger),
   devTools: process.env.NODE_ENV !== "production", // Enable Redux DevTools in development
 });
 
